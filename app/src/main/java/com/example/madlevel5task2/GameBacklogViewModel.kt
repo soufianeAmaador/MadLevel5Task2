ed40ed1d3d3a7c20111cd418c40e044032cd8311
S. Amaador <soufiane.amaador@hva.nl>
2020-11-13 19:14:12 +0100

    viewmodel gemaakt voor game backlog

--- app/src/main/java/com/example/madlevel5task2/GameBacklogViewModel.kt
+++ app/src/main/java/com/example/madlevel5task2/GameBacklogViewModel.kt
@@ -1,2 +1,32 @@
 package com.example.madlevel5task2
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+public class GameBacklogViewModel  (application: Application) : AndroidViewModel(application) {
+
+    private val ioScope = CoroutineScope(Dispatchers.IO)
+    private val gameBacklogRepository = GameBacklogRepository(application.applicationContext)
+
+    val backlogs: LiveData<List<GameBacklog>> = gameBacklogRepository.getGameBacklog()
+
+    fun insertGameBacklog(backlog: GameBacklog) {
+        ioScope.launch { gameBacklogRepository.insertGameBacklog(backlog) }
+    }
+
+    fun deleteGameBacklog(backlog: GameBacklog) {
+        ioScope.launch {
+            gameBacklogRepository.deleteGameBacklog(backlog)
+        }
+    }
+
+    fun updateGameBacklog(backlog: GameBacklog) {
+        ioScope.launch {
+            gameBacklogRepository.updateGameBacklog(backlog)
+        }
+    }
+}
