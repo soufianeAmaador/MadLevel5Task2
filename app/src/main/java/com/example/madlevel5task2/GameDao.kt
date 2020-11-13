a2fb065fc18aaeb3a22436f97a7f31cfbfb3236d
S. Amaador <soufiane.amaador@hva.nl>
2020-11-13 18:44:53 +0100

    GameDao gemaakt

---
+++ app/src/main/java/com/example/madlevel5task2/GameDao.kt
@@ -0,0 +1,20 @@
+package com.example.madlevel5task2
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface GameDao {
+
+    @Insert
+    suspend fun insertGame(game: Game)
+
+    @Query("SELECT * FROM gameTable")
+    fun getGames(): LiveData<List<Game?>>
+
+    @Update
+    suspend fun updateGame(game: Game)
+
+    @Delete
+    suspend fun deleteGame(game: Game)
+}
